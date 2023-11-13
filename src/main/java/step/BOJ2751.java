fde578daca647b667f85139a65481b8c863d4b4e
aqrms <aquarimus@gmail.com>
2023-11-13 23:53:57 +0900

    Solve 2751

---
+++ src/main/java/step/BOJ2751.java
@@ -0,0 +1,32 @@
+package step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BOJ2751 {
+	public void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(list);
+
+		for (int val : list) {
+			sb.append(val).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	public static void main(String[] args) throws IOException {
+		new BOJ2751().solution();
+	}
+}

cc8378e5d4beca0f262a40467b614ab5f42bd502
aqrms <aquarimus@gmail.com>
2023-12-06 00:21:53 +0900

    Solve 11651

--- src/main/java/step/BOJ11651.java
+++ src/main/java/step/BOJ11651.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ11650 {
+public class BOJ11651 {
 
 	public void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,27 +15,30 @@ public class BOJ11650 {
 		int[][] arr = new int[N][2];
 
 		StringTokenizer st;
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(arr, (a1, a2) -> {
-			if (a1[0] == a2[0]) {
-				return a1[1] - a2[1];
-			} else {
-				return a1[0] - a2[0];
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[1] == e2[1]) {
+				return e1[0] - e2[0];
 			}
+			return e1[1] - e2[1];
 		});
+
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
 			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
 		}
+
 		System.out.println(sb);
+
 	}
 
 	public static void main(String[] args) throws IOException {
-		new BOJ11650().solution();
+		new BOJ11651().solution();
 	}
 }

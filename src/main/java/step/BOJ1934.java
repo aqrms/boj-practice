7a8240dae0af5b542a4cbeac37e7af5580585011
aqrms <aquarimus@gmail.com>
2023-12-29 19:34:25 +0900

    Solve 1934

---
+++ src/main/java/step/BOJ1934.java
@@ -0,0 +1,39 @@
+package step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1934 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int gcd = gcd(a, b);
+
+			sb.append(a * b / gcd).append('\n');
+		}
+
+		System.out.println(sb);
+
+	}
+
+	static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+
+}

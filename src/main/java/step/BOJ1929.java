7830ed570d9f58199787c1ff9639d8ba250cc198
aqrms <aquarimus@gmail.com>
2024-01-10 00:22:31 +0900

    Solve 1929

---
+++ src/main/java/step/BOJ1929.java
@@ -0,0 +1,30 @@
+package step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1929 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+
+		for (int i = M; i <= N; i++) {
+			boolean flag = true;
+			for (int j = 2; j <= Math.sqrt(i); j++) {
+				if (i % j == 0) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				System.out.println(i);
+			}
+		}
+
+	}
+}

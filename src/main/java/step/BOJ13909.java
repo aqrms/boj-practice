19ef00cca5bb038ff86e6b3d1ebdad9df51a8b5f
aqrms <aquarimus@gmail.com>
2024-01-17 00:39:16 +0900

    Solve 13909

---
+++ src/main/java/step/BOJ13909.java
@@ -0,0 +1,11 @@
+package step;
+
+import java.util.Scanner;
+
+public class BOJ13909 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println((int)(Math.sqrt(N)));
+	}
+}

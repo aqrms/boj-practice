16497487424c29c60972a22176a28cbfbf2e64ea
aqrms <aquarimus@gmail.com>
2024-02-11 21:58:52 +0900

    Solve 28279

--- src/main/java/step/BOJ28279.java
+++ src/main/java/step/BOJ28279.java
@@ -3,74 +3,56 @@ package step;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BOJ28279 {
+	private static Deque<String> deque = new LinkedList();
+	private static StringTokenizer st;
+	private static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Deque<String> deque = new LinkedList();
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
-		for (int i = 0; i < N; i++) {
+		while (N-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			String command = st.nextToken();
-			switch (command) {
-				case "1":
-					deque.offerFirst(st.nextToken());
-					break;
-				case "2":
-					deque.offerLast(st.nextToken());
-					break;
-				case "3":
-					String poll1 = deque.poll();
-					if (poll1 != null) {
-						sb.append(poll1).append('\n');
-					} else {
-						sb.append("-1").append('\n');
-					}
-					break;
-				case "4":
-					String pollLast = deque.pollLast();
-					if (pollLast != null) {
-						sb.append(pollLast).append('\n');
-					} else {
-						sb.append("-1").append('\n');
-					}
-					break;
-				case "5":
-					sb.append(deque.size()).append('\n');
-					break;
-				case "6":
-					if (deque.isEmpty()) {
-						sb.append("1").append('\n');
-					} else {
-						sb.append("0").append('\n');
-					}
-					break;
-				case "7":
-					String peekFirst = deque.peekFirst();
-					if (peekFirst != null) {
-						sb.append(peekFirst).append('\n');
-					} else {
-						sb.append("-1").append('\n');
-					}
-					break;
-				case "8":
-					String peekLast = deque.peekLast();
-					if (peekLast != null) {
-						sb.append(peekLast).append('\n');
-					} else {
-						sb.append("-1").append('\n');
-					}
-					break;
-			}
+			operate(command);
 		}
 		System.out.println(sb);
 
+	}
+
+	private static void operate(String command) {
+		String result = "";
+		switch (command) {
+			case "1":
+				deque.offerFirst(st.nextToken());
+				return;
+			case "2":
+				deque.offerLast(st.nextToken());
+				return;
+			case "3":
+				result = deque.isEmpty() ? "-1" : deque.poll();
+				break;
+			case "4":
+				result = deque.isEmpty() ? "-1" : deque.pollLast();
+				break;
+			case "5":
+				result = String.valueOf(deque.size());
+				break;
+			case "6":
+				result = deque.isEmpty() ? "1" : "0";
+				break;
+			case "7":
+				result = deque.isEmpty() ? "-1" : deque.peek();
+				break;
+			case "8":
+				result = deque.isEmpty() ? "-1" : deque.peekLast();
+				break;
+		}
+		sb.append(result).append('\n');
 	}
 }

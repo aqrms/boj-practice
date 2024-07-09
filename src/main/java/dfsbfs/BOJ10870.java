3d5c839c6476903ac75abc11b1e1dc87ea627194
aqrms <aquarimus@gmail.com>
2024-07-10 00:16:25 +0900

    Solve 10870 dp

--- src/main/java/dfsbfs/BOJ10870.java
+++ src/main/java/dfsbfs/BOJ10870.java
@@ -5,20 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ10870 {
+    static int[] dp = new int[21];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int result = fib(n);
-        
-        System.out.println(result);
-
+        init(n);
+        System.out.println(dp[n]);
     }
 
-    public static int fib(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-        return fib(n - 1) + fib(n - 2);
+    public static void init(int n) {
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
     }
 }

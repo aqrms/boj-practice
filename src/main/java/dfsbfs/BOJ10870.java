5dfaf1718e4ec8ee72b6ec066b913c3842412ae8
aqrms <aquarimus@gmail.com>
2024-07-09 23:42:36 +0900

    Solve 10870

---
+++ src/main/java/dfsbfs/BOJ10870.java
@@ -0,0 +1,24 @@
+package dfsbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = fib(n);
+        
+        System.out.println(result);
+
+    }
+
+    public static int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}

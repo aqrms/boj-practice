cde18f8944b08bb3ef8356acde22a3236e5e8adb
aqrms <aquarimus@gmail.com>
2024-11-06 00:00:17 +0900

    Solve 10814

--- src/main/java/step/BOJ10814.java
+++ src/main/java/step/BOJ10814.java
@@ -7,28 +7,30 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ10814 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		String[][] arr = new String[N][2];
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = st.nextToken();
-			arr[i][1] = st.nextToken();
-		}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
 
-		Arrays.sort(arr, (o1, o2) -> {
-			return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
-		});
+        Arrays.sort(arr, (o1, o2) -> {
+            return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+        });
 
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < N; i++) {
-			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
-		}
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
+        }
 
-		System.out.println(sb);
-	}
+        System.out.println(sb);
+
+    }
 }

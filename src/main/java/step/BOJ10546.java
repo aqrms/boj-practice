9d79f2e7f707fd22ce66a13ded0b3585b2e188d8
aqrms <aquarimus@gmail.com>
2024-11-07 23:12:00 +0900

    Solve 10546

--- src/main/java/step/BOJ10546.java
+++ src/main/java/step/BOJ10546.java
@@ -3,32 +3,31 @@ package step;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class BOJ10546 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String answer = "";
 
         int N = Integer.parseInt(br.readLine());
-        String[] player = new String[N];
-        String[] finisher = new String[N - 1];
+        HashMap<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
-            player[i] = br.readLine();
+            String player = br.readLine();
+            map.put(player, map.getOrDefault(player, 0) + 1);
         }
 
         for (int i = 0; i < N - 1; i++) {
-            finisher[i] = br.readLine();
+            String player = br.readLine();
+            map.put(player, map.get(player) - 1);
         }
 
-        Arrays.sort(player);
-        Arrays.sort(finisher);
-
-        int i = 0;
-        for (; i < finisher.length; i++) {
-            if (!player[i].equals(finisher[i]))
-                break;
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0)
+                answer = key;
         }
-        System.out.println(player[i]);
+
+        System.out.println(answer);
     }
 }

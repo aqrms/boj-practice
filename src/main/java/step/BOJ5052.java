079ef6cb6df599f8c6b1eb499f28a907660cc636
aqrms <aquarimus@gmail.com>
2024-11-16 00:50:07 +0900

    Solve 5052

--- src/main/java/step/BOJ5052.java
+++ src/main/java/step/BOJ5052.java
@@ -1,7 +1,36 @@
 package step;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class BOJ5052 {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            String[] arr = new String[N];
+
+            for (int j = 0; j < N; j++) {
+                arr[j] = br.readLine();
+            }
+            Arrays.sort(arr);
+            sb.append(isConsistent(arr)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    public static String isConsistent(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1].startsWith(arr[i])) {
+                return "NO";
+            }
+        }
+        return "YES";
     }
 }
